1a63cef3c11f0ce9a70695d295ce5e14e94c6c24
Majrul Ansari <majrul@gmail.com>
2024-09-11 20:19:48 +0530

    updated login code with cookies for auto login (weak version)

--- MyWebApp/src/main/java/com/ey/servlet/LoginServlet.java
+++ MyWebApp/src/main/java/com/ey/servlet/LoginServlet.java
@@ -1,10 +1,11 @@
 package com.ey.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Base64;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,8 +31,23 @@ public class LoginServlet extends HttpServlet {
 			out.write("<h2>Okay</h2>");
 		else*/
 		
-		if(isValid)
+		if(isValid) {
+			String remember = request.getParameter("remember");
+			if(remember != null && remember.equals("y")) {
+				//TODO: Try using some encryption alg. like bcrypt or sha-1
+				String loginData = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+				Cookie c = new Cookie("loginData", loginData);
+				c.setMaxAge(60 * 60 * 24);
+				response.addCookie(c);
+				/*Cookie c1 = new Cookie("uname", username);
+				c1.setMaxAge(60 * 60 * 24);
+				Cookie c2 = new Cookie("pwd", password);
+				c2.setMaxAge(60 * 60 * 24);
+				response.addCookie(c1);
+				response.addCookie(c2);*/
+			}
 			response.sendRedirect("welcome.html?uname="+username);
+		}
 		else
 			response.sendRedirect("login.html?error=1");
 	}
