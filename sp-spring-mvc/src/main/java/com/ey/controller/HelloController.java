78e738224b8cf5e18abc7808e07b4871a890f342
Majrul Ansari <majrul@gmail.com>
2024-09-28 12:19:17 +0530

    different ways of writing the controller code

--- sp-spring-mvc/src/main/java/com/ey/controller/HelloController.java
+++ sp-spring-mvc/src/main/java/com/ey/controller/HelloController.java
@@ -3,14 +3,38 @@ package com.ey.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
-	public String sayHello(Map map) {
-		map.put("message", "Welcome to Spring MVC");
-		return "hello.jsp";
+	public String sayHello(Map model) {
+		model.put("message", "Welcome to Spring MVC");
+		return "hello.jsp"; //view
 	}
+
+	@RequestMapping("/hello2")
+	public String sayHello2(Model model) {
+		model.addAttribute("message", "Welcome to Spring MVC v2");
+		return "hello.jsp"; //view
+	}
+
+	@RequestMapping("/hello3")
+	public String sayHello3(ModelMap model) {
+		model.addAttribute("message", "Welcome to Spring MVC v3");
+		return "hello.jsp"; //view
+	}
+	
+	@RequestMapping("/hello4")
+	public ModelAndView sayHello4() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", "Welcome to Spring MVC v4");
+		mav.setViewName("hello.jsp");
+		return mav;
+	}
+
 }
